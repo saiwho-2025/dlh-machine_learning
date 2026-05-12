fadffaf1b23e38aaa15ca0beceec35feb24ef149
saiwho-2025 <laura.sai.hu@gmail.com>
2026-05-12 21:37:28 +0200

    edit

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import numpy as np
 
-
-# Extract specific row and column slices from the matrix
-C = np.arange(50).reshape(5, 10); a1, a2, a3 = C[0:1], C[0:4], C[:, 3:7]
-print(a1, a2, a3, sep="\n")
+# Slicing a nested list matrix without modules
+C = [list(range(i, i + 10)) for i in range(0, 50, 10)]
+arr1 = C[0:1], arr2 = C[0:4]
+arr3 = [row[3:7] for row in C]
+print(arr1, arr2, arr3, sep="\n")
