3222ac2b2a1610d2a8fc8f369477e9df4dadc534
saiwho-2025 <laura.sai.hu@gmail.com>
2026-05-12 21:38:58 +0200

    Fix syntax for tuple assignment in slicing
    1. no module imported
    2. 8 lines, last one empty

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -2,6 +2,7 @@
 
 # Slicing a nested list matrix without modules
 C = [list(range(i, i + 10)) for i in range(0, 50, 10)]
-arr1 = C[0:1], arr2 = C[0:4]
+arr1 = C[0:1]
+arr2 = C[0:4]
 arr3 = [row[3:7] for row in C]
 print(arr1, arr2, arr3, sep="\n")
