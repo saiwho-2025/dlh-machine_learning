5aa5164d888782e3adc59fa504be038768355369
saihu <laura.sai.hu@gmail.com>
2026-05-14 19:48:18 +0200

    empty line

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -5,3 +5,4 @@ arr1, arr2, arr3 = arr[:2], arr[-5:], arr[1:6]
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
+ 
