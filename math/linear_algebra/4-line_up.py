9ab89c9fb714410528092aab4e8993acd285e3be
saihu <laura.sai.hu@gmail.com>
2026-05-16 12:24:03 +0200

    autopep8

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -7,5 +7,5 @@ def add_arrays(arr1, arr2):
     if len(arr1) != len(arr2):
         return None
 
-    # add the two arrays together element-wise and return the result
+# add the two arrays together element-wise and return the result
     return [a + b for a, b in zip(arr1, arr2)]
