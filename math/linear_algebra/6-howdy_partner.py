1423aaabdf102fc3f62fb364dc7e577c38561890
saihu <laura.sai.hu@gmail.com>
2026-05-16 13:08:43 +0200

    test

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,9 +1,7 @@
-    #!/usr/bin/env python3  
+#!/usr/bin/env python3     
 """Concatenates two 1D arrays."""
 
 
 def cat_arrays(arr1, arr2): 
-    arr1 = [1, 2, 3]
-
-    arr2 = [4, 5, 6]
+    """Concatenates two 1D arrays."""
     return arr1 + arr2
