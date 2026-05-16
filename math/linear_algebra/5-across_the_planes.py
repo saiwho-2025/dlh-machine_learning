9af16107e61a7622fc6495a8debc92b44ab83c39
saihu <laura.sai.hu@gmail.com>
2026-05-16 12:37:38 +0200

    attribute

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
-def add_arrays(arr1, arr2):
-    if len(arr1) != len(arr2):
+"""add two matrices element-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    """adds two 2D matrices element-wise"""
+    # Check if they have the same number of rows
+    if len(mat1) != len(mat2):
         return None
 
-    return [a + b for a, b in zip(arr1, arr2)]
+    # Handle the edge case of empty matrices
+    if len(mat1) == 0:
+        return []
+
+    # Check if the number of columns matches
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+
+    # Perform nested element-wise addition and return a new matrix
+    return [
+        [a + b for a, b in zip(row1, row2)]
+        for row1, row2 in zip(mat1, mat2)
+    ]
