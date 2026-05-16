3c2e2b2138060fcc9de031d7de3852fa330a599d
saihu <laura.sai.hu@gmail.com>
2026-05-16 13:09:48 +0200

    whitesapace=fix

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3     
+#!/usr/bin/env python3
 """Concatenates two 1D arrays."""
 
 
-def cat_arrays(arr1, arr2): 
+def cat_arrays(arr1, arr2):
     """Concatenates two 1D arrays."""
     return arr1 + arr2
