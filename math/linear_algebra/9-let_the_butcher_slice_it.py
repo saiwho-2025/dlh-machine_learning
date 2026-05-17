411aba585b4acb5c45c05873404ce741e0359d2e
saihu <laura.sai.hu@gmail.com>
2026-05-17 14:52:22 +0200

    slice me

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Slice a matrix."""
-import numpy as np
-matrix = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10],
-                   [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
-                   [21, 22, 23, 24, 25]])
+matrix = [[1, 2, 3, 4, 5],
+          [6, 7, 8, 9, 10],
+          [11, 12, 13, 14, 15],
+          [16, 17, 18, 19, 20],
+          [21, 22, 23, 24, 25]]
 mat1 = matrix[1:3]  # Middle two rows
-mat2 = matrix[:, 1:3]  # Middle two columns
-mat3 = matrix[2:, 2:]  # Bottom-right 3x3 matrix
+mat2 = [matrix[0][1:3], matrix[1][1:3], matrix[2][1:3], matrix[3][1:3], matrix[4][1:3]]
+mat3 = [matrix[2][2:], matrix[3][2:], matrix[4][2:]]
