993a62e9ff6612c7d76e49d30896594b14b3416f
saihu <laura.sai.hu@gmail.com>
2026-05-17 14:45:17 +0200

    creation

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""using numpy to muliple 2 matrices"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """returns the product of 2 matrices"""
+    return np.matmul(mat1, mat2)
