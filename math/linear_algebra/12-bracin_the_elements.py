538bd24714fb070a1f8396d1efddf903b46d6173
saihu <laura.sai.hu@gmail.com>
2026-05-17 14:25:34 +0200

    simple

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 """Element-wise matrix operations."""
-import numpy as np
 
 
 def np_elementwise(mat1, mat2):
     """Return element-wise sum, difference, product, and quotient."""
-    mat1 = np.array(mat1)
-    mat2 = np.array(mat2)
-
     return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
