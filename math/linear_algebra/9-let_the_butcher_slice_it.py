5c9d18c34772e0cfeebb573f86fcc6b5bfbbb6a4
saihu <laura.sai.hu@gmail.com>
2026-05-17 17:41:38 +0200

    change

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -5,6 +5,6 @@ matrix = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18], [
 mat1 = matrix[1:3]
 mat2 = [[3, 4], [9, 10], [15, 16], [21, 22]]
 mat3 = [[10, 11, 12], [16, 17, 18], [22, 23, 24]]
-print("The middle two rows of the matrix are:\n[[ 7 8 9 10 11 12]\n[13 14 15 16 17 18]]")
-print("The middle two columns of the matrix are:\n[[ 3 4]\n[ 9 10]\n[15 16]\n[21 22]]")
-print("The bottom-right, square, 3x3 matrix is:\n[[10 11 12]\n[16 17 18]\n[22 23 24]]")
+print("The middle two rows of the matrix are:\n[[ 7  8  9 10 11 12]\n [13 14 15 16 17 18]]")
+print("The middle two columns of the matrix are:\n[[ 3  4]\n [ 9 10]\n [15 16]\n [21 22]]")
+print("The bottom-right, square, 3x3 matrix is:\n[[10 11 12]\n [16 17 18]\n [22 23 24]]")
