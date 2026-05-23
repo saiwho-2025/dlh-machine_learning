f0f7b1986ccd953e63ce6bd378dad1ff078f45ea
saihu <laura.sai.hu@gmail.com>
2026-05-23 20:38:20 +0200

    end of function

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -1,6 +1,7 @@
---ranks country origins of bands, by number of (non-unique)fans
+cat > 15-fans.sql << 'EOF'
 -- Rank country origins by total number of fans
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
+EOF
