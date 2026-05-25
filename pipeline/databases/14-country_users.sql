bf8517e0dfbd4301b901d84f8b11ba4e5e4cdcb3
saihu <laura.sai.hu@gmail.com>
2026-05-25 10:35:02 +0200

    create database

--- pipeline/databases/14-country_users.sql
+++ pipeline/databases/14-country_users.sql
@@ -1,8 +1,8 @@
--- create table users with country enumeration
+-- Create users table with country enumeration
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
-    email INT NOT NULL AUTO_INCREMENT,
-    NAME VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
     PRIMARY KEY (id)
 );
