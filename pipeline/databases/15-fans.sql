4b12fd32ab32e2098025175294591d5094aa9c8f
saihu <laura.sai.hu@gmail.com>
2026-05-25 10:47:32 +0200

    create

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -1,5 +1,6 @@
--- Rank country origins by total number of fans
-SELECT origin, SUM(fans) AS nb_fans
+-- List Glam rock bands ranked by lifespan
+SELECT band_name,
+       IF(split IS NULL, 2020, split) - formed AS `lifespan until 2020 (in years)`
 FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+WHERE style LIKE '%Glam rock%'
+ORDER BY `lifespan until 2020 (in years)` DESC;
