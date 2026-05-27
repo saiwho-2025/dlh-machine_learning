a828abf9d3e2b2caedf91df52194a35cd1bb8a68
saihu <laura.sai.hu@gmail.com>
2026-05-27 13:11:43 +0200

    average_weighted_score

---
+++ pipeline/databases/103-average_weighted_score.sql
@@ -0,0 +1,16 @@
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (
+        SELECT SUM(c.score * p.weight) / SUM(p.weight)
+        FROM corrections AS c
+        INNER JOIN projects AS p
+            ON c.project_id = p.id
+        WHERE c.user_id = user_id
+    )
+    WHERE id = user_id;
+END //
+
+DELIMITER ;
