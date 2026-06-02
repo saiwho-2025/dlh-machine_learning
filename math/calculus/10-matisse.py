34d4a177f0b190e9f683beb7cd7990f8484afc2d
saihu <laura.sai.hu@gmail.com>
2026-06-02 21:06:03 +0200

    difinite purpose

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""this module calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """the function"""
+    if _poly is not valid;
+        return None
+    
+    if _derivative == 0;
+        return [0]
+    
+    poly_derivative(poly) = 
