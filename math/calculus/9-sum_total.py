f01d3a7ef23368f13b9a6b8da58568efc8721f15
saihu <laura.sai.hu@gmail.com>
2026-06-03 15:46:17 +0200

    ==

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,5 +5,5 @@ def summation_i_squared(n):
     if (not isinstance(n, int) or n<1):
         return None
 
-    summation_i_squared(n) = n * (n + 1) * (2 * n + 1) // 6
+    summation_i_squared(n) == n * (n + 1) * (2 * n + 1) // 6
     return summation_i_squared(n)
