4d03ce65102d94995a847ce81851f7a1cbb887f3
saihu <laura.sai.hu@gmail.com>
2026-06-03 15:45:02 +0200

    update

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,7 +2,8 @@
 """this module caculates the sum of numbers from 1 to n to their power of 2"""
 
 def summation_i_squared(n):
-    if (not isinstance(n, int) or n<1);
-return None
+    if (not isinstance(n, int) or n<1):
+        return None
 
-return n * (n + 1) * (2 * n + 1) // 6
+    summation_i_squared(n) = n * (n + 1) * (2 * n + 1) // 6
+    return summation_i_squared(n)
