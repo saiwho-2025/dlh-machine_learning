0f157efa38d496514f59fc8696558742de8114ce
saihu <laura.sai.hu@gmail.com>
2026-06-06 15:12:13 +0200

    r-

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 
 
 def line():
+    """This module plots y as a line graph."""
     y = np.arange(0, 11) ** 3
+
     plt.figure(figsize=(6.4, 4.8))
-    x = np.arange(0,10)
-    plt.plot(x, y, color ='red', linestyle = 'solid')
+    plt.xlim(0,10)
+    plt.plot(y, color ='red', linestyle = 'solid')
