8c7bb951b9b2b9dbe390fd736f63ed17fb457f3d
saihu <laura.sai.hu@gmail.com>
2026-06-06 17:47:02 +0200

    yes see the graph

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -15,7 +15,6 @@ def scatter():
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
 
-
     plt.figure(figsize=(6.4, 4.8))
     plt.scatter(x, y, color="magenta")
     plt.xlabel("Height (in)")
