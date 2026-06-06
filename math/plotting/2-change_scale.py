2af43d712a8c8a59787fec9f02d04e7f8a849dfe
saihu <laura.sai.hu@gmail.com>
2026-06-06 18:05:12 +0200

    creation

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""This module plot a line graph"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlabel("Time(years)")
+    plt.ylabel("Fraction Remaining")
+    plt.title("Exponential Decay of C-14")
+    plt.yscale("log")
+    plt.xlim(0, 28651)
+
+    plt.show
