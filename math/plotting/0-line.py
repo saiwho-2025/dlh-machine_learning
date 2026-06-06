6ac5f73a8bff489f11417c7563e8af297cff977b
saihu <laura.sai.hu@gmail.com>
2026-06-06 14:25:05 +0200

    creation

---
+++ math/plotting/0-line.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""this module complete the source code to ploy y as a line graph"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    x = np.arange(0,10)
+    plt.plot(x, y, color ='red', linestyle = 'solid')
