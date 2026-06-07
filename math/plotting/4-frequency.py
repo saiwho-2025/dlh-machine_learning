ad0678e0cd54d32759603be243de1e46c57fe1c6
saihu <laura.sai.hu@gmail.com>
2026-06-07 20:49:56 +0200

    pass not

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -8,12 +8,15 @@ def frequency():
     """Frenquency groupped into bins, blue ones"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
+
+    # initialize the graph
     plt.figure(figsize=(6.4, 4.8))
 
-    # your code here
+    # Plot the bins
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.xlim(0, 100)
+
+    # show the result
     plt.show()
