71535086e653ac2f8590cb54cc9a3e38b4612170
saihu <laura.sai.hu@gmail.com>
2026-06-07 15:50:26 +0200

    correction

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 def frequency():
     # plot student grades for a project
 
-    np.random.seed(5)
+    np.random.seed(0)
     student_grades = np.random.normal(68, 15, 50)
 
     # Initialize the figure size
@@ -16,9 +16,6 @@ def frequency():
 
     # Define bins every 10 units from 0 to 100
     bins = np.arange(0, 101, 10)
-
-    # Initialize the figure size
-    plt.figure(figsize=(6.4, 4.8))
 
     # Plot histogram with black outlines around the bars
     plt.hist(student_grades, bins=bins, edgecolor='black')
