d1dd14f3b9e51c37422e8b3b0da553eb8d5831fe
saihu <laura.sai.hu@gmail.com>
2026-06-07 16:34:15 +0200

    xlim

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -27,7 +27,7 @@ def frequency():
 
     # Align the x-axis ticks with the 10-unit bins and set precise limits
     plt.xticks(bins)
-    plt.xlim(0, 100)
 
     # Display the final graph
     plt.show()
+
