81bd82e3cd33762b3abd832d80b505849e2efe74
saihu <laura.sai.hu@gmail.com>
2026-06-14 14:45:54 +0200

    Probability Density Function

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -20,3 +20,12 @@ class Exponential:
             raise ValueError("data must contain multiple values")
 
         self.lambtha = float(len(data) / sum(data))
+
+    def pdf(self, x):
+        """Calculate the value of the PDF for a given time period."""
+        if x < 0:
+            return 0
+        
+        e = 2.7182818285
+
+        return self.lambtha *(e **(-self.lambtha * x))
