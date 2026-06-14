8708cd48b021995f40044c997a918ba67fe69ffc
saihu <laura.sai.hu@gmail.com>
2026-06-14 12:56:51 +0200

    in block

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -16,10 +16,11 @@ class Exponential:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
         self.lambtha = float(lambtha)
-        
-        if not isinstance(data, list):
+
+        else:
+            if not isinstance(data, list):
                 raise TypeError("data must be a list")
-        if len(data) < 2:
+            if len(data) < 2:
             raise ValueError("data must contain multiple values")
         
         self.lambtha = float(len(data) / sum(data))
