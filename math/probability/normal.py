361652993a3442ba2087d54b25b300bcef6eab3b
saihu <laura.sai.hu@gmail.com>
2026-06-14 17:12:35 +0200

    fix indentation

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-"""Using class Normal to represent an exponential distribution."""
+"""Using class Normal to represent a normal distribution."""
 
 
 class Normal:
     """The classic Normal distribution.
-        data is a list of the data to be used to estimate the distribution
-        mean is the mean of the distribution
-        stddev is the standard deviation of the distribution"""
+
+    data is a list of the data to be used to estimate the distribution
+    mean is the mean of the distribution
+    stddev is the standard deviation of the distribution
+    """
 
     def __init__(self, data=None, mean=0., stddev=1.):
-        """Initialize the Exponential distribution."""
+        """Initialize the Normal distribution."""
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
+
             self.mean = float(mean)
             self.stddev = float(stddev)
 
@@ -25,10 +28,9 @@ class Normal:
 
             self.mean = float(sum(data) / len(data))
 
-        total = 0
-
-        for x in data:
-            total += (x - self.mean) ** 2
+            total = 0
+            for x in data:
+                total += (x - self.mean) ** 2
 
             variance = total / len(data)
             self.stddev = float(variance ** 0.5)
