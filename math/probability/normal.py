e696e0a5c29961f5a11828ec454aec3243f1cfd8
saihu <laura.sai.hu@gmail.com>
2026-06-14 17:53:28 +0200

    cdf

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -52,3 +52,15 @@ class Normal:
         exponent = -0.5 * ((x - self.mean) / self.stddev) ** 2
 
         return coefficient * (e ** exponent)
+
+    def cdf(self, x):
+        """Calculates the value of the CDF for a given x-value"""
+        pi = 3.1415926536
+
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
+
+        erf = (2 / (pi ** 0.5)) * (
+            z - (z ** 3) / 3 + (z ** 5) / 10 -
+            (z ** 7) / 42 + (z ** 9) / 216)
+
+        return (1 + erf) / 2
