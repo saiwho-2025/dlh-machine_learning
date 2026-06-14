c054e6ec98f4061707bd19e1a54d431f72d58391
saihu <laura.sai.hu@gmail.com>
2026-06-14 17:50:18 +0200

    fix 1

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -44,10 +44,11 @@ class Normal:
         return float((z * self.stddev) + self.mean)
 
     def pdf(self, x):
-        """Calculates the PDF of a given x"""
-        e = 2.7182818285
+        """Calculates the value of the PDF for a given x-value"""
         pi = 3.1415926536
+        e = 2.7182818285
 
+        coefficient = 1 / (self.stddev * ((2 * pi) ** 0.5))
+        exponent = -0.5 * ((x - self.mean) / self.stddev) ** 2
 
-        exponent = -0.5 * ((x - self.mean / self.stddev)) ** 2
-        return (1 / (self.stddev * ((2* pi) ** 0.5))) * (e ** exponent)
+        return coefficient * (e ** exponent)
