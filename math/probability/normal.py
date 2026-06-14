61c757d6b7b921563cb6295216d5d1cdee04bb51
saihu <laura.sai.hu@gmail.com>
2026-06-14 17:39:32 +0200

    pdf - Probability Density Function

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -42,3 +42,12 @@ class Normal:
     def x_value(self, z):
         """Calculates the x-value of a given z-score"""
         return float((z * self.stddev) + self.mean)
+
+    def pdf(self, x):
+        """Calculates the PDF of a given x"""
+        e = 2.7182818285
+        pi = 3.1415926536
+
+
+        exponent = -0.5 *((x - self.mean / self.stddev)) ** 2
+        return (1/ (self.stddev * ((2* pi) ** 0.5))) * (e ** exponent)
