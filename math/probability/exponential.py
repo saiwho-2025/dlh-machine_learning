609a8ebe33fd237fe62ecd8041b3fc0cb5c22dc3
saihu <laura.sai.hu@gmail.com>
2026-06-14 13:00:23 +0200

    fix1

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -11,16 +11,15 @@ class Exponential:
         data is a list of data for estimating the distribution
         lambtha is the expected number of occurences in a given time frame
         """
-    
+
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
         self.lambtha = float(lambtha)
 
-        else:
-            if not isinstance(data, list):
-                raise TypeError("data must be a list")
-            if len(data) < 2:
+        if not isinstance(data, list):
+            raise TypeError("data must be a list")
+        if len(data) < 2:
             raise ValueError("data must contain multiple values")
-        
+
         self.lambtha = float(len(data) / sum(data))
