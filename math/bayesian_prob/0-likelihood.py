8b171460b76018bf91fc9e500e0cccbcf18b4689
saihu <laura.sai.hu@gmail.com>
2026-06-23 16:33:10 +0200

    creation

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""this module to calculate the likelihood"""
+import numpy as np
+
+def likelihood(x,n,P):
+    """Arg:
+    x is the number of patients that develop severe side effects
+    n is the total number of patients observed
+    P is a 1D numpy.ndarray containing the various 
+    hypothetical probabilities of developing severe side effects
+    
+"""
+    if n is not int or n <= 0: 
+        raise ValueError("n must be a positive integer")
+    
+    if x is not int or x < 0:
+        raise ValueError("x must be an integer that is greater than "
+        "or equal to 0")
+    
+    if not isinstance (P,np.dyarray):
+        raise TypeError("p must be a 1D numpy.ndarray")
+    
+    if not np.all((P >= 0)& (P <= 1)):
+        raise ValueError("All values in P must be in the range [0,1]")
+
+
+    return likelihood(x,n,np.each(P))
