cfec29a6bbd7bbf8e4550457ad4a9dc89dbb9c22
saihu <laura.sai.hu@gmail.com>
2026-06-23 17:16:56 +0200

    fix

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -10,18 +10,18 @@ def likelihood(x,n,P):
     hypothetical probabilities of developing severe side effects
     
 """
-    if n is not int or n <= 0: 
-        raise ValueError("n must be a positive integer")
+    if type(n) is not int or n <= 0: 
+         raise ValueError("n must be a positive integer")
     
-    if x is not int or x < 0:
-        raise ValueError("x must be an integer that is greater than "
+    if type(x) is not int or x < 0:
+        raise  ValueError("x must be an integer that is greater than "
         "or equal to 0")
     
-    if not isinstance (P,np.dyarray):
-        raise TypeError("p must be a 1D numpy.ndarray")
+    if not isinstance (P,np.ndarray):
+        raise TypeError("P must be a 1D numpy.ndarray")
     
     if not np.all((P >= 0)& (P <= 1)):
         raise ValueError("All values in P must be in the range [0,1]")
 
 
-    return likelihood(x,n,np.each(P))
+    return likelihood(x,n,np.any(P))
