771a7910d2775e78996eff2baba6eebc956a5b1e
saihu <laura.sai.hu@gmail.com>
2026-06-23 21:50:42 +0200

    indentation

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -23,11 +23,11 @@ def likelihood(x, n, P):
         raise ValueError("n must be a positive integer")
 
     if not isinstance(x, int):
-        raise ValueError("x must be an integer that is"
-                        " greater than or equal to 0")
+        raise ValueError("x must be an integer that is "
+                         "greater than or equal to 0")
     if x < 0:
         raise ValueError("x must be an integer that is "
-                        "greater than or equal to 0")
+                         "greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
 
