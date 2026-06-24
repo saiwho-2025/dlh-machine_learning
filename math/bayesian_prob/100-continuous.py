319cdd86454b49bb452f5aac2580b145f4165e1b
saihu <laura.sai.hu@gmail.com>
2026-06-24 22:33:48 +0200

    submit

---
+++ math/bayesian_prob/100-continuous.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Calculates the Posterior probabilities of data."""
+
+from scipy import special
+
+
+def intersection(x, n, p1, p2):
+    """
+    Calculates the likelihood of obtaining the data given
+    various hypothetical probabilities of developing severe side effects.
+
+    Args:
+        x: number of patients who develop severe side effects
+        n: to  tal number of patients observed
+        P1: the lower bound on the range
+        P2: the upper bound on the range
+
+    Returns:
+        the posterior probability of the intersection
+    """
+    if not isinstance(n, int):
+        raise ValueError("n must be a positive integer")
+    if n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int):
+        raise ValueError("x must be an integer that is "
+                         "greater than or equal to 0")
+    if x < 0:
+        raise ValueError("x must be an integer that is "
+                         "greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(p1, float) or p1 < 0 or p1 > 1: 
+        raise ValueError(" {p} must be a float in the range [0, 1]" 
+        " where {p} is the corresponding variable ")
+
+    if not isinstance(p2, float) or p2 <0 or p2 >1:
+        raise ValueError(" {p} must be a float in the range [0, 1]" 
+        " where {p} is the corresponding variable ")
+
+    if p2 <= p1:
+        raise ValueError("p2 must be greater than p1")
+
+    a = x + 1
+    b = n - x + 1
+
+    return special.betainc(a, b, p2) - special.betainc(a, b, p1)
