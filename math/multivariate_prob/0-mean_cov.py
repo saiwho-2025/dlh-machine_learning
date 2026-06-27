841f6ef28852fe9185bab0ed59a25032f89ca428
saihu <laura.sai.hu@gmail.com>
2026-06-27 20:58:16 +0200

    kayword nospace

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -11,15 +11,14 @@ def mean_cov(X):
         d is the number of dimensions in each data point
     """
 
-    if not isinstance(X, np.ndarray) or X.ndim !=2:
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
-    
+
     n,d = X.shape
 
     if n < 2: 
         raise ValueError("X must contain multiple data points")
-    
-    mean = np.mean(X, axis = 0, keepdims = True)
-    cov = np.matmul((X - mean).T, (X - mean))/(n-1)
 
+    mean = np.mean(X, axis=0, keepdims=True)
+    cov = np.matmul((X - mean).T, (X - mean))/(n-1)
     return mean, cov
