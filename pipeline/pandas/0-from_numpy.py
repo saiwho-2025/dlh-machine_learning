fb4a7a17cf90fe75c17bb573bfaa5a5463449a3b
saihu <laura.sai.hu@gmail.com>
2026-07-07 13:56:09 +0200

    creation

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+
+def from_numpy(array):
+    """Create a DataFrame from a NumPy ndarray."""
+    columns = [chr(65 + i) for i in range(array.shape[1])]
+    return pd.DataFrame(array,columns=columns)
