c2dc77a38230742cf9d078d538cd566d01d976a2
saihu <laura.sai.hu@gmail.com>
2026-07-07 13:57:54 +0200

    creation

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -7,4 +7,4 @@ import pandas as pd
 def from_numpy(array):
     """Create a DataFrame from a NumPy ndarray."""
     columns = [chr(65 + i) for i in range(array.shape[1])]
-    return pd.DataFrame(array,columns=columns)
+    return pd.DataFrame(array, columns=columns)
