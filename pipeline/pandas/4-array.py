9ed3ea77570a7db7c1edc3e0b3ba7ef256135693
saihu <laura.sai.hu@gmail.com>
2026-07-10 20:03:03 +0200

    remove the module

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """take a pd.DataFrame as input, select function, convert into ndarray"""
 
-import pandas as pd
-
 
 def array(df):
     """Return the last 10 rows of High and Close as a NumPy array."""
