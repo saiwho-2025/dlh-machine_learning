1d156c5f7df53ef673fee5d0832a1e3825df780d
saihu <laura.sai.hu@gmail.com>
2026-07-10 20:49:45 +0200

    fix

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -3,5 +3,6 @@
 
 
 def slice(df):
-    """take a pd.DataFrame, extract columns, select the 60th row and return it"""
+    """take a pd.DataFrame, extract columns,
+    select the 60th row and return it"""
     return df[["High", "Low", "Close", "Volume_(BTC)"]].iloc[::60]
