da9810393b7f470ff70a50f53e3b43b06ee860e8
saihu <laura.sai.hu@gmail.com>
2026-07-11 13:23:39 +0200

    creation

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""remove column and fill row blank in a pd.DataFrame"""
+
+
+def fill(df):
+    """edit the pd.DataFrame following requirements"""
+    df.drop(column = "Weighted_Price")
+    df["Close"] = df["Close"].ffill()
+
+    df["High"] = df["High"].fillna(df["Close"])
+    df["Low"] = df["Low"].fillna(df["Close"])
+    df["Open"] = df["Open"].fillna(df["Close"])
+
+    df["Volume_(BTC)"] = df["Volume_(BTC)"].fillna(0)
+    df["Volume_(Currency)"] = df["Volume_(Currency)"].fillna(0)
+
+    return df
