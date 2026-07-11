63c1e148110ec4bf328410be72657f4c845e15d2
saihu <laura.sai.hu@gmail.com>
2026-07-11 21:54:30 +0200

    creation

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Concatenate two DataFrames using Timestamp as index."""
+
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """Concatenate selected bitstamp data above coinbase data."""
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2 = df2.loc[:1417411920]
+
+    return pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
