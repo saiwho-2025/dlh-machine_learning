1ef65b2113dee2566646f83542945e2050c93ff6
saihu <laura.sai.hu@gmail.com>
2026-07-11 14:04:36 +0200

    columns syntax

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -4,7 +4,7 @@
 
 def fill(df):
     """edit the pd.DataFrame following requirements"""
-    df = df.drop(column = ["Weighted_Price"])
+    df = df.drop(columns = ["Weighted_Price"])
     df["Close"] = df["Close"].ffill()
 
     df["High"] = df["High"].fillna(df["Close"])
