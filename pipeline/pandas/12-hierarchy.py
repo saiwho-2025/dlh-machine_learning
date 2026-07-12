446bd41a08da5e5a1f517d3cc741aa59396b812d
saihu <laura.sai.hu@gmail.com>
2026-07-12 23:46:13 +0200

    creation

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""layers and order the work based on previous work"""
+
+import pandas as pd
+index = __import__('10-index').index
+
+
+def hierarchy(df1, df2):
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df1 = df1.loc[1417411980:1417417980]
+    df2 = df2.loc[1417411980:1417417980]
+
+    df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
+    return df.swaplevel().sort_index
