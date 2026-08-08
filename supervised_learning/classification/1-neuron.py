310b68a653ff8c62a95cf0dcd68d0132e2f293f7
saihu <laura.sai.hu@gmail.com>
2026-08-08 13:08:08 +0200

    creation private attributes

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""defines a single neuron for binary classification"""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron for binary classification."""
+
+    def __init__(self, nx):
+        """arg: nx is the number of input features to the neuron
+            publice instance attributes:
+            w is the weight vector for the neuron,
+            initialized using a random normal distribution
+            b is the bias for the neuron, initialized to 0
+            A is the activated output of the neuron(prediction),
+            initialized to 0"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
