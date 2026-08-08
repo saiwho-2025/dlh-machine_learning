78b9f6c047be0b0af62376722e1a7b5351394526
saihu <laura.sai.hu@gmail.com>
2026-08-08 13:00:11 +0200

    correction system indentation

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""this module defines a single neuron performing binary classification using OOP"""
+"""defines a single neuron for binary classification"""
 
 import numpy as np
 
@@ -8,17 +8,17 @@ class Neuron:
     """Defines a single neuron for binary classification."""
 
 
-def __init__(self, nx):
-    """arg: nx is the number of input features to the neuron
+    def __init__(self, nx):
+        """arg: nx is the number of input features to the neuron
             publice instance attributes:
             w is the weight vector for the neuron, initialized using a random normal distribution
             b is the bias for the neuron, initialized to 0
             A is the activated output of the neuron(prediction), initialized to 0"""
-    if not isinstance(nx, int):
-        raise TypeError("nx must be an integer")
-    if nx < 1:
-        raise ValueError("nx must be an positive integer")
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
 
-    self.W = np.random.normal(size=(1, nx))
-    self.b = 0
-    self.A = 0
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
