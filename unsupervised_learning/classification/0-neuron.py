51328b11811f32c24daf63221f2fea811bab1ee8
saihu <laura.sai.hu@gmail.com>
2026-08-08 07:27:01 +0200

    update folder relocation

---
+++ unsupervised_learning/classification/0-neuron.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""this module defines a single neuron performing binary classification using OOP"""
+
+
+def _init_(self,nx):
+    """arg: nx is the number of input features to the neuron
+            publice instance attributes:
+            w is the weight vector for the neuron, initialized using a random normal distribution
+            b is the bias for the neuron, initialized to 0
+            A is the activated output of the neuron(prediction), initialized to 0"""
+    if not isinstance (nx,int):
+        raise TypeError ("nx must be an integer")
+    if nx < 1:
+        raise ValueError ( "nx must be an positive integer")
+
+    self.W = np.random.normal(size=(1,nx))
+    self.b = 0
+    self.A = 0
