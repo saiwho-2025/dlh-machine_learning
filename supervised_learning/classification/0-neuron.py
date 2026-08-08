12ae05bb96ff245af16340113645af9d51f35d6a
saihu <laura.sai.hu@gmail.com>
2026-08-08 12:45:09 +0200

    creation

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -3,21 +3,22 @@
 
 import numpy as np
 
+
 class Neuron:
     """Defines a single neuron for binary classification."""
 
 
-def _init_(self,nx):
+def _init_(self, nx):
     """arg: nx is the number of input features to the neuron
             publice instance attributes:
             w is the weight vector for the neuron, initialized using a random normal distribution
             b is the bias for the neuron, initialized to 0
             A is the activated output of the neuron(prediction), initialized to 0"""
-    if not isinstance (nx,int):
-        raise TypeError ("nx must be an integer")
+    if not isinstance(nx, int):
+        raise TypeError("nx must be an integer")
     if nx < 1:
-        raise ValueError ( "nx must be an positive integer")
+        raise ValueError("nx must be an positive integer")
 
-    self.W = np.random.normal(size=(1,nx))
+    self.W = np.random.normal(size=(1, nx))
     self.b = 0
     self.A = 0
