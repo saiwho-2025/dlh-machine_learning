29ad3b37cd13360977eb081d05ec3e2c4d61c1ca
saihu <laura.sai.hu@gmail.com>
2026-08-31 22:30:49 +0200

    fix

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Trains a neural network and analyzes validation data."""
 
+import tensorflow.keras as K
+
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
