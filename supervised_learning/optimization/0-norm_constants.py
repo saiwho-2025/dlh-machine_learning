d69cc2fc23ebdc58c0d81b9614ac26f6f9c9cd56
saihu <laura.sai.hu@gmail.com>
2026-09-02 13:47:08 +0200

    creation

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def normalization_constants(X):
+    """
+    Calculates the normalization (standardization) constants
+    of a matrix.
+
+    X: numpy.ndarray of shape (m, nx)
+       m = number of data points
+       nx = number of features
+
+    Returns:
+        mean: numpy.ndarray of shape (nx,)
+        std: numpy.ndarray of shape (nx,)
+    """
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+
+    return mean, std
