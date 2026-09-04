4a0f73126b0c714d94388c82820b60cce21ed23c
saihu <laura.sai.hu@gmail.com>
2026-09-04 11:39:23 +0200

    fix

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -1,21 +1,20 @@
-import numpy as np
-
-
 #!/usr/bin/env python3
 """this module calculates the weighted moving average of a data set"""
 
+import numpy as np
 
-def moving_average(data,beta):
+
+def moving_average(data, beta):
     """this module trains by the weighted moving average
     args:
-        data: list of data to calculate the moving average 
+        data: list of data to calculate the moving average
         beta: the weight used for the moving average"""
     averages = []
-    v =  0
+    v = 0
 
-    for t, value in enumerate(data,1):
-        v = beta * v + (1- beta) * value
-        v_corrected = v / (1- beta ** t)
+    for t, value in enumerate(data, 1):
+        v = beta * v + (1 - beta) * value
+        v_corrected = v / (1 - beta ** t)
         averages.append(v_corrected)
 
     return averages
