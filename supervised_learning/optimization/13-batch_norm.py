42eee6c83c7e70145030532ce16159315e1ad612
saihu <laura.sai.hu@gmail.com>
2026-09-04 14:44:24 +0200

    fix

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def batch_norm(Z, gamma, beta, epsilon):
     """
-    Normalizes an unactivated output of a neural network using batch normalization.
+    batch normalization an unactivated output of a neural network.
     """
     mean = np.mean(Z, axis=0, keepdims=True)
     variance = np.var(Z, axis=0, keepdims=True)
