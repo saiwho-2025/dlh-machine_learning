dc85d78e82706575a18f6d2d6635c7d2b05c8f20
saihu <laura.sai.hu@gmail.com>
2026-09-15 21:15:54 +0200

    creation

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Calculates the sensitivity for each class."""
+
+import numpy as np
+
+def sensitivity(confusion):
+"""Calculates the sensitivity for each class.
+Args:
+    confusion: A confusion numpy.ndarray of shape (classes, classes)
+        where row indices represent the correct labels and column
+        indices represent the predicted labels.
+Returns:
+    A numpy.ndarray of shape (classes,) containing the sensitivity
+    of each class.
+"""
+# Get the true positives from the diagonal.
+true_positives = np.diag(confusion)
+
+# Calculate the total number of actual examples for each class.
+actual = np.sum(confusion, axis=1)
+
+# Calculate sensitivity for each class.
+return true_positives / actual
