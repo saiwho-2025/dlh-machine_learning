e0f6947390ef754c1275cbc964f3ff31b3be695c
saihu <laura.sai.hu@gmail.com>
2026-09-18 23:23:50 +0200

    backpropagation use the current weight matrix

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""L2 regularization gradient descent module."""
+"""L2 regularization gradient descent."""
 
 import numpy as np
 
@@ -13,16 +13,17 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     for layer in range(L, 0, -1):
         A_prev = cache["A{}".format(layer - 1)]
 
-        dw = (1 / m) * np.matmul(dz, A_prev.T)
+        dw = np.matmul(dz, A_prev.T) / m
         dw += (lambtha / m) * weights["W{}".format(layer)]
 
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
+        db = np.sum(dz, axis=1, keepdims=True) / m
 
-        weights["W{}".format(layer)] -= alpha * dw
-        weights["b{}".format(layer)] -= alpha * db
-
+        # Calculate dz for the previous layer BEFORE updating W
         if layer > 1:
             dz = np.matmul(
                 weights["W{}".format(layer)].T,
                 dz
             ) * (1 - np.square(A_prev))
+
+        weights["W{}".format(layer)] -= alpha * dw
+        weights["b{}".format(layer)] -= alpha * db
