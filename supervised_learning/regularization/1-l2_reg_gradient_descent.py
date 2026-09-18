d6b3e866f7b50273eab517e67ef3968816c0634a
saihu <laura.sai.hu@gmail.com>
2026-09-18 23:25:54 +0200

    pycodestyle

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -5,7 +5,8 @@ import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """Updates weights and biases using gradient descent with L2 regularization."""
+    """Updates weights and biases using
+    gradient descent with L2 regularization."""
     m = Y.shape[1]
 
     dz = cache["A{}".format(L)] - Y
