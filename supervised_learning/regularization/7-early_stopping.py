610d1fb5871d7934a82a621ad0f4f5156f5c26ac
saihu <laura.sai.hu@gmail.com>
2026-09-19 09:35:16 +0200

    create

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -4,12 +4,12 @@
 
 def early_stopping(cost, opt_cost, threshold, patience, count):
     """Determines if gradient descent should stop early."""
-    if cost > opt_cost - threshold:
-        count += 1
-    else:
+    if cost < opt_cost - threshold:
         count = 0
+    else:
+        count += 1
 
-    if count >= patience:
+    if count > patience:
         return True, count
 
     return False, count
