3b05ff11882b9f47d375b5784e540732ccfb63df
saihu <laura.sai.hu@gmail.com>
2026-09-20 16:55:53 +0200

    create

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""This module randomly adjusts the contrast of an image."""
+"""This module randomly changes the brightness of an image."""
 
 import tensorflow as tf
 
 
-def change_contrast(image, lower, upper):
-    """Randomly adjust the contrast of an image."""
-    return tf.image.random_contrast(image, lower=lower, upper=upper)
+def change_brightness(image, max_delta):
+    """Randomly change the brightness of an image."""
+    return tf.image.random_brightness(image, max_delta)
