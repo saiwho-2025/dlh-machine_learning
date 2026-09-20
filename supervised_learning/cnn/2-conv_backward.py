1aecac89489093e251dc9fafbfa5cee406bdd88e
saihu <laura.sai.hu@gmail.com>
2026-09-20 14:07:08 +0200

    a_slice

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -11,7 +11,6 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     sh, sw = stride
     h_new, w_new = dZ.shape[1], dZ.shape[2]
 
-    # Calculate padding
     if padding == "same":
         pad_h = max((h_new - 1) * sh + kh - h_prev, 0)
         pad_w = max((w_new - 1) * sw + kw - w_prev, 0)
@@ -20,13 +19,16 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
         pad_bottom = pad_h - pad_top
         pad_left = pad_w // 2
         pad_right = pad_w - pad_left
-    else:
+
+    elif padding == "valid":
         pad_top = 0
         pad_bottom = 0
         pad_left = 0
         pad_right = 0
 
-    # Pad A_prev
+    else:
+        raise ValueError("padding must be 'same' or 'valid'")
+
     A_prev_pad = np.pad(
         A_prev,
         (
@@ -38,42 +40,64 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
         mode="constant"
     )
 
-    # Initialize gradients
     dA_prev_pad = np.zeros_like(A_prev_pad)
     dW = np.zeros_like(W)
     db = np.zeros_like(b)
 
-    # Back propagation
     for i in range(m):
         for h in range(h_new):
             for w in range(w_new):
                 h_start = h * sh
-                h_end = h_start + kh
-
                 w_start = w * sw
+
+                h_end = h_start + kh
                 w_end = w_start + kw
+
+                # Clip the window to the padded input
+                h_start_clip = max(h_start, 0)
+                h_end_clip = min(h_end, A_prev_pad.shape[1])
+                w_start_clip = max(w_start, 0)
+                w_end_clip = min(w_end, A_prev_pad.shape[2])
+
+                # Corresponding kernel region
+                kh_start = h_start_clip - h_start
+                kh_end = kh_start + (h_end_clip - h_start_clip)
+                kw_start = w_start_clip - w_start
+                kw_end = kw_start + (w_end_clip - w_start_clip)
 
                 a_slice = A_prev_pad[
                     i,
-                    h_start:h_end,
-                    w_start:w_end,
+                    h_start_clip:h_end_clip,
+                    w_start_clip:w_end_clip,
                     :
                 ]
 
                 for c in range(c_new):
                     dz = dZ[i, h, w, c]
 
-                    dW[:, :, :, c] += a_slice * dz
+                    dW[
+                        kh_start:kh_end,
+                        kw_start:kw_end,
+                        :,
+                        c
+                    ] += a_slice * dz
+
                     db[:, :, :, c] += dz
 
                     dA_prev_pad[
                         i,
-                        h_start:h_end,
-                        w_start:w_end,
+                        h_start_clip:h_end_clip,
+                        w_start_clip:w_end_clip,
                         :
-                    ] += W[:, :, :, c] * dz
+                    ] += (
+                        W[
+                            kh_start:kh_end,
+                            kw_start:kw_end,
+                            :,
+                            c
+                        ] * dz
+                    )
 
-    # Remove padding
     dA_prev = dA_prev_pad[
         :,
         pad_top:pad_top + h_prev,
