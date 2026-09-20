f7004f2ed6939c86137a28895efe87b536a57fd2
saihu <laura.sai.hu@gmail.com>
2026-09-20 15:21:28 +0200

    fix

--- pipeline/data_augmentation/0-flip.py
+++ pipeline/data_augmentation/0-flip.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-"""this module flip an image horizontally from left to right."""
+"""This module flips an image horizontally from left to right."""
+
+import tensorflow as tf
 
 
 def flip_image(image):
+    """Flip an image horizontally."""
     return tf.image.flip_left_right(image)
